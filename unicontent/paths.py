4c06a162a8086a058e5a1be0504daef2e6d8591f
hboisgibault <h.boisgibault@gmail.com>
2017-03-14 17:57:58 +0700

    Minor bug fixes

--- unicontent/paths.py
+++ unicontent/paths.py
@@ -18,7 +18,6 @@ class HtmlPath(Path):
 
     def find_element(self, soup):
         element = soup.find(self.tag_name, attrs=self.attributes)
-        print(element)
         if not element:
             return False
         if not self.content_name:
